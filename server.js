59d68b2fccade87663def47336dad8c05bc239ff
Sneha Singh <snehasingh240903@gmail.com>
2026-04-15 07:39:15 +0530

    fix cors + api url

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors({
-  origin: ["https://code-and-conquer-saas-11g6.onrender.com"],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
