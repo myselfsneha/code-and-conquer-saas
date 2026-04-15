82d11d3e778cb122c9e4a964fd0ec0ad74f5d065
Sneha Singh <snehasingh240903@gmail.com>
2026-04-15 07:24:54 +0530

    fix db connection final

--- server.js
+++ server.js
@@ -22,7 +22,13 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "frontend")));
 /* ================= DATABASE ================= */
 
-const db = mysql.createConnection(process.env.MYSQLPUBLICURL);
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
 
 
 db.connect((err) => {
